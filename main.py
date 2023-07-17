f8db3b4cab6e27938e6d61c408db2f52ab04f1c0
shahabbai <shahab.cv.ai@gmail.com>
2023-07-17 09:56:22 +0330

    better output visualization

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-video_writer = cv2.VideoWriter('output/' + output_videoname, fourcc, fps, (frame_width, frame_height))
+
 if total_frames > 1:
+    video_writer = cv2.VideoWriter('output/' + output_videoname, fourcc, fps, (frame_width, frame_height))
     #do inference for video
     while cap.isOpened():
         success, img = cap.read()
@@ -44,7 +45,11 @@ if total_frames > 1:
                     cv2.rectangle(img,(x1, y1), (x2, y2), (255, 0, 0), 3)
                     confs = math.ceil((box.conf[0]*100))/100
                     cls_names = int(box.cls[0])
-                    cv2.putText(img, f'{classnames[cls_names]} {confs}', (max(40, x1), max(40, y1)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 20, 255),thickness=1)
+                    if cls_names == 1:
+                        cv2.putText(img, f'{confs}', (max(40, x2 + 5), max(40, y2 + 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(0, 20, 255),thickness=1, lineType=cv2.LINE_AA)
+                    elif cls_names == 0:
+                        cv2.putText(img, f'{confs}', (max(40, x1), max(40, y1)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.6, color=(0, 20, 255),thickness=1, lineType=cv2.LINE_AA)
+                    
 
                     #check plate to recognize characters with yolov8n model
                     if cls_names == 1:
@@ -67,7 +72,6 @@ if total_frames > 1:
                         #convert all characters to a string
                         for i in sorted_list:
                             char_class = i[0]
-                            #char_display.append(plate_output[0].names[char_class])
                             char_display.append(charclassnames[char_class])
                         char_result ='Plate: ' + (''.join(char_display))
                         fps_text_2 = "FPS: {:.2f}".format(1/elapsed_time_2)
@@ -75,13 +79,14 @@ if total_frames > 1:
             
                         #just show the correct characters in output
                         if len(char_display) == 8:
-                            cv2.putText(img, char_result , (40,40), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1.2, color=(10, 50, 255),thickness=2)
+                            cv2.line(img, (max(40, x1 - 25 ), max(40, y1 - 10)), (x2 + 25 ,y1 - 10), (0, 0, 0), 20,lineType=cv2.LINE_AA)
+                            cv2.putText(img, char_result , (max(40, x1 - 15), max(40, y1 - 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(10, 50, 255),thickness=1, lineType=cv2.LINE_AA)
                 tock = time.time()
                 elapsed_time = tock - tick
                 fps_text = "FPS: {:.2f}".format(1/elapsed_time)
                 text_size, _ = cv2.getTextSize(fps_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
                 fps_text_loc = (frame_width - text_size[0] - 10, text_size[1] + 10)
-                cv2.putText(img, fps_text , fps_text_loc, fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(10, 50, 255),thickness=2)
+                cv2.putText(img, fps_text , fps_text_loc, fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(10, 50, 255),thickness=2, lineType=cv2.LINE_AA)
 
             cv2.imshow('detection', img)
             video_writer.write(img)
@@ -103,10 +108,13 @@ else: #do inference for image
         for box in bbox:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            cv2.rectangle(img,(x1, y1), (x2, y2), (255, 0, 0), 3)
+            cv2.rectangle(img,(x1, y1), (x2, y2), (255, 0, 0), 1)
             confs = math.ceil((box.conf[0]*100))/100
             cls_names = int(box.cls[0])
-            cv2.putText(img, f'{classnames[cls_names]} {confs}', (max(40, x1), max(40, y1)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 20, 255),thickness=1)
+            if cls_names == 1:
+                cv2.putText(img, f'{confs}', (max(40, x2 + 5), max(40, y2 + 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(0, 20, 255),thickness=1, lineType=cv2.LINE_AA)
+            elif cls_names == 0:
+                cv2.putText(img, f'{confs}', (max(40, x1), max(40, y1)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.6, color=(0, 20, 255),thickness=1, lineType=cv2.LINE_AA)
 
             #check plate to recognize characters with yolov8n model
             if cls_names == 1:
@@ -129,11 +137,12 @@ else: #do inference for image
                     char_class = i[0]
                     #char_display.append(plate_output[0].names[char_class])
                     char_display.append(charclassnames[char_class])
-                char_result ='Plate: ' + (''.join(char_display))
+                char_result = (''.join(char_display))
     
                 #just show the correct characters in output
                 if len(char_display) == 8:
-                    cv2.putText(img, char_result , (40,40), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1.2, color=(10, 50, 255),thickness=2)
+                    cv2.line(img, (max(40, x1 - 25 ), max(40, y1 - 10)), (x2 + 25 ,y1 - 10), (0, 0, 0), 20,lineType=cv2.LINE_AA)
+                    cv2.putText(img, char_result , (max(40, x1 - 15), max(40, y1 - 5)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.5, color=(10, 50, 255),thickness=1, lineType=cv2.LINE_AA)
 
     cv2.imshow('detection', img)
     cv2.imwrite('output/' + output_imagename, img)
@@ -142,9 +151,4 @@ else: #do inference for image
         cap.release()
         cv2.destroyAllWindows()
 
-
-    
- 
-    
-        
 
